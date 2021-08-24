afd98e3bb745b501b6bcf616b1093166a4200a95
Eduardo Medeiros Pereira <edu.medeirospereira@gmail.com>
2021-08-24 22:11:25 +0100

    creating all files and folder structure needed by the actions framework
    Update version file to version:hash 0.0.0:a89bb11

---
+++ src/action.js
@@ -0,0 +1,48 @@
+const exec = require('@actions/exec');
+const fs = require('fs');
+const path = require('path');
+
+async function executeCommand(command, args=[]) {
+
+    let myOutput = '';
+    let myError = '';
+
+    const options = {};
+    options.listeners = {
+        stdout: (data) => {
+            myOutput += data.toString();
+        },
+        stderr: (data) => {
+            myError += data.toString();
+        }
+    };
+
+    await exec.exec(command, args, options);
+
+    return myOutput;
+
+}
+
+
+async function run(){
+
+    let version = await executeCommand("git",["tag","--sort","-v:refname"]) || "0.0.0";
+    let hash = await executeCommand("git", ["rev-parse","--short","HEAD"]);
+    let fileContent = `${version}:${hash}`;
+
+    let versionFilePath = path.join(process.env.GITHUB_WORKSPACE, 'version');
+
+    fs.writeFileSync(versionFilePath,fileContent);
+
+    await executeCommand("git",["config","--global","user.name","AZOS Actions Version File"]);
+
+    await executeCommand("git",["config","--global","user.email","azos.version.action@github.com"]);
+
+    await executeCommand("git",["add","version"]);
+
+    await executeCommand("git",["commit","-m",`Update version file to version:hash ${fileContent}`]);
+
+    await executeCommand("git",["push"]);
+}
+
+run();
