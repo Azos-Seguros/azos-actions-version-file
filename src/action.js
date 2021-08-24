d361bdf7b9c86b9016ef45ff6ccf98fac30b644a
Eduardo Medeiros Pereira <edu.medeirospereira@gmail.com>
2021-08-24 23:32:46 +0100

    testttt ttt

--- src/action.js
+++ src/action.js
@@ -1,5 +1,14 @@
+var shell = require('shelljs');
+
 async function run(){
-    console.log('TESTE TEST');
+
+    let version = await shell.exec('node --version', {silent:true}).stdout;
+    let hash = await shell.exec('node --version', {silent:true}).stdout;
+    let fileContent = `${version}:${hash}`;
+
+    console.log( `Version: ${fileContent}`)
+
+    await shell.exec(`echo ${fileContent} > version`, {silent:true});
 }
 
 run();
