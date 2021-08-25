c1ee327b80c60efa015fb4f40587de1ae0aece5c
Eduardo Medeiros Pereira <edu.medeirospereira@gmail.com>
2021-08-26 00:04:12 +0100

    asdfasdf

--- dist/index.js
+++ dist/index.js
@@ -1410,7 +1410,9 @@ async function run(){
     //executeCommand(`cd ${process.env.GITHUB_WORKSPACE}`);
 
     let version = await executeCommand("git",["tag","--sort","-v:refname"]);
-    let hash = await executeCommand("git", ["log"," --oneline","|","awk","'NR==1{print $1}'"]);
+    console.log("version===",version);
+    let hash = await executeCommand("git", ["log","--oneline","|","awk","'NR==1{print $1}'"]);
+    console.log("hash===",hash);
     let fileContent = `${version}:${hash}`;
 
     console.log( `Version: ${fileContent}`)
