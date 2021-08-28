a0957f396d8f53bc299231ebfb05db824f6fac3e
Eduardo Medeiros Pereira <edu.medeirospereira@gmail.com>
2021-08-28 01:05:18 +0100

    asdf ssss

--- src/action.js
+++ src/action.js
@@ -27,22 +27,12 @@ async function executeCommand(command, args=[]) {
 async function run(){
 
     let version = await executeCommand("git",["tag","--sort","-v:refname"]) || "0.0.0";
-    console.log("version===",version);
     let hash = await executeCommand("git", ["rev-parse","--short","HEAD"]);
-    console.log("hash===",hash);
     let fileContent = `${version}:${hash}`;
 
     let versionFilePath = path.join(process.env.GITHUB_WORKSPACE, 'version');
 
-    console.log("versionFilePath",versionFilePath);
-
-    console.log( `Version: ${fileContent}`)
-
-    await executeCommand("pwd",null);
-
-    await executeCommand("echo",[`${fileContent}`,">","version"]);
-
-    await executeCommand("cat",["version"]);
+    fs.writeFileSync(versionFilePath,fileContent);
 
     await executeCommand("git",["add","version"]);
 
